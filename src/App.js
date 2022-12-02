e1420188aa0dac0b27b64ae5b9650b86ae590e3d
Meet Guleria <meetguleria94@gmail.com>
2022-12-02 17:40:07 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
+import noteService from './Services/notes'
+import Note from './Components/Note'
 function App() {
+
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
+
+  useEffect(() => {
+    noteService
+      .getAll()
+      .then(initialNotes => {
+        setNotes(initialNotes)
+      })
+  }, [])
+
+  const addNote = (e) => {
+    e.preventDefault()
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() > 0.5,
+    }
+
+    noteService
+      .create(noteObject)
+      .then(returnedNote => {
+        setNotes(notes.concat(returnedNote))
+        setNewNote('')
+      })
+  }
+
+  const handleNoteChange = (e) => {
+    setNewNote(e.target.value)
+  }
+
+  const notesToShow = showAll ? notes : notes.filter(note => note.important === true)
+
+  const toggleImportanceOf = (id) => {
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important}
+
+    noteService
+      .update(id, changedNote)
+      .then(returnedNote => {
+        setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+    })
+      .catch(error => {
+        alert(`the note '${note.content}' was already deleted from server.`)
+        setNotes(notes.filter(n => n.id !== id))
+      })
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all'}
+        </button>
+      </div>
+      <ul>
+        {notesToShow.map((note) => {
+          return (
+            <div>
+              <Note 
+                key={note.id} 
+                note={note} 
+                toggleImportance={() => toggleImportanceOf(note.id)}
+              />
+            </div>
+          )
+        })}
+      </ul>
+      <form onSubmit={addNote}>
+        <input 
+          value={newNote}
+          onChange={handleNoteChange}
+        />
+        <button type='submit'>Save</button>
+      </form>
     </div>
   );
 }
---
+++ src/Components/Note.js
@@ -0,0 +1,14 @@
+function Note({note, toggleImportance}) {
+
+  const label = note.important ? 'make not important' : 'make important'
+  return (
+    <div>
+        <li>
+          {note.content}
+          <button onClick={toggleImportance}>{label}</button>
+        </li>
+    </div>
+  )
+}
+
+export default Note
